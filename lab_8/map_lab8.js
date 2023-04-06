32a15c4cc84d3a39a14bf9e14e950ad514f52244
emmatrettin <123512343+emmatrettin@users.noreply.github.com>
2023-04-06 14:20:41 -0500

    Add files via upload

---
+++ lab_8/map_lab8.js
@@ -0,0 +1,71 @@
+
+$(document).ready(function() {
+    $("#btn1").click(function(){
+        $("#splasher1").show();
+    });
+    $("#btn2").click(function(){
+        $("#splasher1").hide();
+    });
+    $("#btn3").click(function(){
+        $("#splasher2").fadeIn();
+    });
+    $("#btn4").click(function(){
+        $("#splasher2").fadeOut();
+    });
+});
+
+var mymap = L.map('map', {
+    center: [38.85250119246977, -99.60815961021089],
+    zoom: 4,
+});
+
+var streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ2NoYXVkaHVyaSIsImEiOiJjazBtcG5odG8wMDltM2JtcjdnYTgyanBnIn0.qwqjMomdrBMG36GQKXBlMw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1
+}).addTo(mymap);
+
+var icon = L.icon({
+    iconUrl: 'img/icon.png',
+    iconSize: [20,20],
+    iconAnchor: [2.5,2.5],
+    popupAnchor: [0,-11],
+});
+
+var cities = L.esri.featureLayer({
+    url: 'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_Major_Cities_/FeatureServer/0',
+    pointToLayer: function (geojson, latlng) {
+            return L.marker(latlng, {
+              icon: icon
+            });
+          }
+        }).addTo(mymap);
+
+cities.bindPopup(function (layer) {
+        return L.Util.template(
+          "<p><strong>{NAME}</strong>, {ST}</p>",
+          layer.feature.properties
+        );
+      });
+
+var wildfireRisk = L.esri.dynamicMapLayer({
+    url: 'https://maps7.arcgisonline.com/arcgis/rest/services/USDA_USFS_2014_Wildfire_Hazard_Potential/MapServer',
+    f: 'image'
+  }).addTo(mymap);
+
+wildfireRisk.bindPopup(function (error, featureCollection) {
+    if (error || featureCollection.features.length === 0) {
+      return false;
+    } else {
+      return 'Risk Level: ' + featureCollection.features[0].properties.CLASS_DESC;
+    }
+  });
+
+var overlays = {
+    'Wildfires': wildfireRisk,
+    'Major US Cities': cities,
+}
+var layerControl = L.control.layers({}, overlays, {collapsed: false}).addTo(mymap);
